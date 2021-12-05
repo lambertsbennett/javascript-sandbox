6230c1a8b6ce26c78972109a3601c5f7bab36b11
lambertsbennett <lambertsbennett@gmail.com>
2021-12-05 13:48:53 +0100

    add react intro

---
+++ ReactIntro/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+      </div>
+      <div className="expense-item__price">Amount</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
